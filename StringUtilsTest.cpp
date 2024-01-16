684aa0741450ceafda724da3bc480f91cc700469
jdkriner <jackkriner@icloud.com>
2024-01-15 17:05:51 -0800

    finished makefile and half of tests written

--- StringUtilsTest.cpp
+++ StringUtilsTest.cpp
@@ -6,27 +6,54 @@ TEST(StringUtilsTest, SliceTest){
 }
 
 TEST(StringUtilsTest, Capitalize){
+    ASSERT_EQ("", StringUtils::Capitalize(""));
+    ASSERT_EQ("Capitalized string", StringUtils::Capitalize("capitalized string"));
+    ASSERT_EQ("Capital", StringUtils::Capitalize("cApItal"));
+    ASSERT_EQ("Caps", StringUtils::Capitalize("CAPS"));
+    ASSERT_EQ("Letter", StringUtils::Capitalize("Letter"));
     
 }
 
 TEST(StringUtilsTest, Upper){
+    ASSERT_EQ("", StringUtils::Upper(""));
+    ASSERT_EQ("UPPER", StringUtils::Upper("upper"));
+    ASSERT_EQ("UPPERCASE", StringUtils::Upper("UPPERCASE"));
+    ASSERT_EQ("UPPER CHARS", StringUtils::Upper("Upper chars"));
     
 }
 
 TEST(StringUtilsTest, Lower){
+    ASSERT_EQ("", StringUtils::Lower(""));
+    ASSERT_EQ("lower", StringUtils::Lower("LOWER"));
+    ASSERT_EQ("lowercase", StringUtils::Lower("lower"));
+    ASSERT_EQ("lower chars", StringUtils::Lower("LoWEr CHars"));
     
 }
 
 TEST(StringUtilsTest, LStrip){
+    ASSERT_EQ("", StringUtils::LStrip(""));
+    ASSERT_EQ("", StringUtils::LStrip("   "));
+    ASSERT_EQ("Hello", StringUtils::LStrip("   Hello"));
+    ASSERT_EQ("NoLeft   ", StringUtils::LStrip("NoLeft   "));
+    ASSERT_EQ("Tableft", StringUtils::LStrip("  Tableft"));
     
 }
 
 TEST(StringUtilsTest, RStrip){
+    ASSERT_EQ("", StringUtils::RStrip(""));
+    ASSERT_EQ("", StringUtils::RStrip("   "));
+    ASSERT_EQ("Hello", StringUtils::RStrip("Hello   "));
+    ASSERT_EQ("   NoRight", StringUtils::RStrip("   NoRight"));
+    ASSERT_EQ("Tabright", StringUtils::RStrip("Tabright "));
     
 }
 
 TEST(StringUtilsTest, Strip){
-    
+    ASSERT_EQ("", StringUtils::Strip(""));
+    ASSERT_EQ("", StringUtils::Strip("   "));
+    ASSERT_EQ("Hello", StringUtils::Strip("   Hello   "));
+    ASSERT_EQ("NoSpaces", StringUtils::Strip("NoSpaces"));
+    ASSERT_EQ("Tabs", StringUtils::Strip("  Tabs    "));
 }
 
 TEST(StringUtilsTest, Center){
--- makefile
+++ makefile
@@ -1,20 +1,27 @@
 INCLUDE = -I/usr/src/gtest
 LIBPATH = -L/usr/lib
+OBJ = obj
+BIN = bin
+
+all: subdir $(BIN)/teststrutils runbinfile
 
 subdir:
 	@mkdir -p obj
 	@mkdir -p bin
 
-teststrutils: StringUtils.o StringUtilsTest.o
+$(BIN)/teststrutils: $(OBJ)/StringUtils.o $(OBJ)/StringUtilsTest.o
 	@echo "Generating executable"
-	g++ StringUtils.o StringUtilsTest.o -o teststrutils $(INCLUDE) $(LIBPATH) -lgtest -lgtest_main -pthread
+	g++ $(OBJ)/StringUtils.o $(OBJ)/StringUtilsTest.o -o $(BIN)/teststrutils $(INCLUDE) $(LIBPATH) -lgtest -lgtest_main -pthread
 
-StringUtils.o: StringUtils.cpp StringUtils.h
-	g++ -c StringUtils.cpp -o StringUtils.o
+$(OBJ)/StringUtils.o: StringUtils.cpp StringUtils.h
+	g++ -c StringUtils.cpp -o $(OBJ)/StringUtils.o
 
-StringUtilsTest.o: StringUtilsTest.cpp StringUtils.h
-	g++ -c StringUtilsTest.cpp -o StringUtilsTest.o
+$(OBJ)/StringUtilsTest.o: StringUtilsTest.cpp StringUtils.h
+	g++ -c StringUtilsTest.cpp -o $(OBJ)/StringUtilsTest.o
+
+runbinfile:
+	./bin/teststrutils
 	
 clean:
-	@rmdir obj
-	@rmdir bin
+	@rm -r obj
+	@rm -r bin
